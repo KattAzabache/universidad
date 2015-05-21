1cc879e25b1106c4b0b201f055fc2a84456738e0
katty-azabache <kattyazabachez@gmail.com>
2015-05-21 02:32:09 -0500

    complete el cuadro completo de cursos

--- mi-script.js
+++ mi-script.js
@@ -1,3 +1,10 @@
-$(".menu-collapsed").click(function() {
-  $(this).toggleClass("menu-expanded");
-});
+$(document).ready(function() {
+	$("..icon").click(function(event){
+	event.preventDefault();
+		$("dentro-ojo").toggleClass("hidden");
+    	$(".icon").toggleClass("hidden");
+	});
+/*$(".icon").click(function() {
+   $(".icon").toggleClass("hidden");
+   $(".dentro-ojos").toggleClass("hidden")
+});*/
